30e666290801dc9e77b250fb17405998d0b8771d
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-07-27 00:48:43 +0700

    refactor: folder structure

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-require('dotenv').config();
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose'
 
-const app = express();
-const port = process.env.PORT || 5000;
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.status(200);
-  res.json({
-    status: 'Healthy'
+async function main() {
+  // Connect Database
+  await mongoose.connect(process.env.DATABASE_URL);
+
+  const app = express();
+  const port = process.env.PORT || 5000;
+  
+  app.get('/', (req, res) => {
+    res.status(200);
+    res.json({
+      status: 'Healthy'
+    })
   })
-})
+  
+  app.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`);
+  });
+}
 
-app.listen(port, () => {
-  console.log(`Server running on port http://localhost:${port}`);
-});
+main().catch((err) => console.log(err));
