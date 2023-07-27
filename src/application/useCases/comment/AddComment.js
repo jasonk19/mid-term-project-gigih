932d94e4993679de9c1f95de228080c21d6997e2
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-07-27 23:31:38 +0700

    fix: add comment cannot be empty

--- src/application/useCases/comment/AddComment.js
+++ src/application/useCases/comment/AddComment.js
@@ -1,6 +1,12 @@
 import commentDatabase from "../../../infrastructure/database/comment/CommentDatabase.js";
 
 export async function addCommentUseCase({accountId, videoId, comment}) {
+  if (comment === '') {
+    const error = new Error('Comment cannot be empty');
+    error.statusCode = 400;
+    throw error;
+  }
+
   await commentDatabase.addComment({
     accountId,
     videoId,
