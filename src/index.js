c58286d7d8b999eb730503a4d1067ccbd9d1641f
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-08-12 16:30:02 +0700

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bodyparser from 'body-parser';
+import cors from 'cors';
 
 import { accountRouter } from './api/routers/AccountRouter.js';
 import { productRouter } from './api/routers/ProductRouter.js';
@@ -16,6 +17,7 @@ async function main() {
   const app = express();
   const port = process.env.PORT || 5000;
   
+  app.use(cors())
   app.use(bodyparser.json())
   app.use(bodyparser.urlencoded({ extended: false }))
 
